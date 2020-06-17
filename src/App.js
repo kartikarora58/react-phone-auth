38fa59d218d3a07241db11942c53c896b7feb9c9
Kartik <kartikarora58@gmail.com>
2020-06-17 13:26:29 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebase=require('firebase');
+class App extends Component
+{
+  constructor(props)
+  {
+      super(props)
+      this.state={
+        phone:null,
+      }
+  }
+  componentDidMount() {
+      window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier("recaptcha-container",{
+        size:'invisible',
+      });
+  }
+  handelChange=(e)=>{
+    const {value}=e.target;
+    this.setState({
+      phone:value,
+    })
+  }
+  handleSubmit=()=>{
+    // let recaptcha=new firebase.auth.RecaptchaVerifier('recaptcha');
+    let number='+91'+this.state.phone;
+    let captcha=window.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber(number,captcha)
+      .then(function(e){
+        let code=prompt("enter OTP");
+        e.confirm(code)
+          .then(result=>console.log(result.user.getIdToken()))
+          .catch(err=>console.log(err))
+      })
+      .catch(error=>console.log(error))
+  }
+  
+  render()
+  {
+    return (
+      <div className="App">
+        <div id="recaptcha"></div>
+          <input 
+          type="text" 
+          onChange={(e)=>this.handelChange(e)}
+          />
+          <button id="recaptcha-container" onClick={()=>this.handleSubmit()}>submit</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+const firebase=require('firebase')
+const firebaseConfig={
+  apiKey: "AIzaSyC45t6uhcW0wFr82PrC9qXaISubasSWJag",
+  authDomain: "chat-6a3e7.firebaseapp.com",
+  databaseURL: "https://chat-6a3e7.firebaseio.com",
+  projectId: "chat-6a3e7",
+  storageBucket: "chat-6a3e7.appspot.com",
+  messagingSenderId: "850390706140",
+  appId: "1:850390706140:web:cf829e1d4d4eefcfd2d119",
+  measurementId: "G-NELHPRMTV7"
+};
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
